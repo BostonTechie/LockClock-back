1840dbd0202455422fb06814f9fb17a5a34c6b98
Andrew D Urquhart <85643280+BostonTechie@users.noreply.github.com>
2022-03-19 12:19:15 -0400

    no weirdness yet

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, g, jsonify
 from flask_cors import CORS
 from flask_login import LoginManager
+from dotenv import load_dotenv
+import os
 
 
 DEBUG = True
@@ -8,11 +10,18 @@ PORT = 8000
 
 app = Flask(__name__)
 
+
+SESSION_SECRET = os.getenv('SESSION_SECRET')
+app.secret_key = SESSION_SECRET 
+
 @app.get('/')
 def test_route():
     return 'App is working'
 
 if __name__ == '__main__':
+
+    print(SESSION_SECRET)
+   
     app.run(
         port=PORT,
         debug=DEBUG
