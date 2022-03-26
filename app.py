0a1d7eec6e6407647d42408c86003181fe352a36
Andrew D Urquhart <85643280+BostonTechie@users.noreply.github.com>
2022-03-26 10:15:42 -0400

    :tada: restarted

--- app.py
+++ app.py
@@ -1,104 +1,64 @@
-from flask import Flask, g, jsonify
+from flask import Blueprint, request, Flask, g, jsonify
+from playhouse.shortcuts import model_to_dict
 from flask_cors import CORS
-from flask_login import LoginManager
-from dotenv import load_dotenv
-import os
+from flask_login import login_user, logout_user
 
 import models
-# import my various controllers where my routes are
-from resources.controllers.users import user
-from resources.controllers.projects import project
-
 
 DEBUG = True
-PORT = 8000 
+PORT = 8000
 
 app = Flask(__name__)
- 
-load_dotenv()
-
-SESSION_SECRET = os.getenv('SESSION_SECRET')
-app.secret_key = SESSION_SECRET 
-
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-@login_manager.unauthorized_handler
-def handle_unauthorized():
-    # this will send back json when we make an unauthorized request
-    # the default is html
-    return jsonify(
-        data={},
-        status=401,
-        message='You must be logged in to do that'
-    ), 401
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    try:
-        return models.User.get_by_id(user_id)
-    except models.DoesNotExist:
-        return None
-    
-CORS(user, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
-CORS(project, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
-CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True) 
-
-
-app.register_blueprint(user, url_prefix='/api/v1/users')
-app.register_blueprint(project, url_prefix='/api/v1/projects')
-
-
-   
-
-@app.before_request
-def before_request():
-    '''Connect to the database before each request.'''
-    g.db = models.DATABASE
-    g.db.connect()
-
-@app.after_request
-def after_request(response):
-    '''Close the database connection after each request.'''
-    g.db.close()
-    return response
-
-
-# the app object has an errorhandler decorator
-
-@app.errorhandler(404)
-def handle_404(err):
-    return jsonify(
-        data={},
-        message='404: Resource not found',
-        status=404
-    ), 404
-
-@app.errorhandler(405)
-def handle_405(err):
-    return jsonify(
-        data={},
-        message='405: Method not allowed',
-        status=405
-    ), 405
-
-@app.errorhandler(500)
-def handle_500(err):
-    return jsonify(
-        data={},
-        message='500: Internal server error',
-        status=500
-    ), 500
-
-
+CORS(app, origins=['http://localhost:3000'], supports_credentials=True)
 
 @app.get('/')
 def test_route():
     return 'App is working'
 
-if __name__ == '__main__':
+@app.get('/get')
+def get_route():
+    all_users = models.User.select()
+    user_dicts = [model_to_dict(user) for user in all_users]
     
+    return jsonify(
+    data= user_dicts,
+    message=f'Fetched {len(user_dicts)} users!',
+    status=200
+    ), 200
+
+@app.post('/register')
+def register():
+
+    payload = request.get_json()
+    print(payload)
+
+    try:
+        models.User.get(models.User.email == payload['email'])
+        # in Peewee we can use .get() to retrieve a single record
+        return jsonify(
+            data={},
+            status=401,
+            message='A user with that email already exists'
+        ), 401
+    except models.DoesNotExist:
+        print(payload)
+        # return "Look at terminal"
+        created_user = models.User.create(**payload)
+       
+        user_dict = model_to_dict(created_user)
+        # let's delete the password before we send it back
+        # it throws an error because it's not serializable
+        # and we don't want to send it back anyways
+  
+        return jsonify(
+            data=user_dict,
+            message='Successfully registered',
+            status=201
+        ), 201
+
+if __name__ == '__main__':
+    # print('Session Secret:', SESSION_SECRET)
+    # we need to initialize our database here
     models.initialize()
     app.run(
         port=PORT,
